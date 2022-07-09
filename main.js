f763b50ec7c13a08960624ea016177a7e2761d29
Diplomat999 <mmmaxxx1987@gmail.com>
2022-07-09 22:10:39 +0300

    corausel

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
-$(document).ready(function(){
-    $('.slider').bxSlider();
+$(document).ready(function () {
+  $('.slider').bxSlider({
+    auto: true,
+    stopAutoOnClick: true,
   });
+});
 
-  $(document).ready(function(){
-    $('.started-slider').bxSlider({
-      controls: false,
-    }
-    );
-  });
+$(document).ready(function () {
+  $('.started-slider').bxSlider({
+    controls: false,
+    auto: true,
+  }
+  );
+});
+
+$(document).ready(function () {
+  $('.partners-slider').bxSlider({
+    minSlides: 1,
+    maxSlides: 3,
+    moveSlides: 3,
+    slideWidth: 170,
+    shrinkItems: true,
+}
+  );
+});
