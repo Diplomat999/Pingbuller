833acd43e48145ddae8993be3f0f7eb0c37756f6
Diplomat999 <mmmaxxx1987@gmail.com>
2022-07-13 23:48:55 +0300

    media

--- main.js
+++ main.js
@@ -1,25 +1,19 @@
 $(document).ready(function () {
-  $('.slider').bxSlider( {
+  $('.slider').bxSlider({
     slideMargin: 40,
 
   });
-});
 
-$(document).ready(function () {
   $('.started-slider').bxSlider({
     controls: false,
     auto: true,
-  }
-  );
-});
+  });
 
-$(document).ready(function () {
   $('.partners-slider').bxSlider({
     minSlides: 1,
     maxSlides: 3,
     moveSlides: 3,
     slideWidth: 170,
     shrinkItems: true,
-}
-  );
+  });
 });
